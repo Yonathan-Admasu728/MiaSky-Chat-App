a9cfbe3a183fc97b5c767b92721f495c87c02281
Yonathan  Admasu <75287408+Yonathan-Admasu728@users.noreply.github.com>
2023-03-14 10:45:20 -0700

    updated the naming of the virtual assistatant

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -8,13 +8,13 @@
 	<div class="chat-image avatar">
 		<div class="w-10 rounded-full">
 			<img
-				src="https://ui-avatars.com/api/?name={type === 'user' ? 'Me' : 'B'}"
+				src="https://ui-avatars.com/api/?name={type === 'user' ? 'Me' : 'Td'}"
 				alt="{type} avatar"
 			/>
 		</div>
 	</div>
 	<div class="chat-header">
-		{type === 'user' ? 'Me' : 'Bot'}
+		{type === 'user' ? 'Me' : 'Tedy'}
 	</div>
 	<div class="chat-bubble {type === 'user' ? 'chat-bubble-secondary' : 'chat-bubble-primary'}">
 		{message}

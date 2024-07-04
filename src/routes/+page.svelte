cb74400a487be6b6bf23d6064aab4df04bd224c0
Yonathan  Admasu <admyoni4@gmail.com>
2024-07-03 21:15:03 -0700

    updated the model

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,7 +65,7 @@
 <div class="flex flex-col pt-4 w-full px-8 items-center gap-2">
 	<div>
 		<h1 class="text-2xl font-bold w-full text-center">Mia-Sky Virtual Assistant</h1>
-		<p class="text-sm italic">Powered by gpt-3.5-turbo</p>
+		<p class="text-sm italic">Powered by gpt-4o-turbo</p>
 	</div>
 	<div class="h-[500px] w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
 		<div class="flex flex-col gap-2">
--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -66,7 +66,7 @@ export const POST: RequestHandler = async ({ request }) => {
 		]
 
 		const chatRequestOpts: CreateChatCompletionRequest = {
-			model: 'gpt-3.5-turbo',
+			model: 'gpt-4o',
 			messages,
 			temperature: 0.9,
 			stream: true
